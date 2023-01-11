c6c37030fe9e19d2438855a7b04a15b0051fefc0
Rifkifz45 <rifkilhokseumawe2484@gmail.com>
2023-01-11 14:55:35 +0700

    ubah

--- HalamanAdmin/proses hapus kelas.php
+++ HalamanAdmin/proses hapus kelas.php
@@ -10,6 +10,6 @@
 	}else{
 		?><script>alert('Hapus data Kelas gagal');</script><?php	
 	}
-	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=dataKelas.php">';
+	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=lhtkelas.php">';
 	
 ?>
